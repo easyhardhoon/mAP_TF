a7f11fb92fae13bfa8a19dafeecbf7b9bcda9106
easyhardhoon <ghoon0040@gmail.com>
2023-10-12 04:23:37 +0000

    Add scripts for DOT

---
+++ evaluate_DOT/DOT_2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for i in {0..20}; do
+  source_folder="./input/DOT/choose_2/DOT_$i"
+  python_script="main.py"
+
+  copy_and_run() {
+    cd "$1"
+    cp -r *.txt ../../../detection-results
+    cd ../../../../
+    python3 "$2" --no-plot
+    echo "Completed processing $1"
+  }
+
+  copy_and_run "$source_folder" "$python_script"
+done
+
